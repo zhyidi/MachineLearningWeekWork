2b71ec976bc37b8f9242fa117c4d2936dfd113c8
zhyidi <zhyidi@vip.qq.com>
2023-11-28 15:36:34 +0800

    zhyidi commit.

--- Week11/k_means.py
+++ Week11/k_means.py
@@ -1,5 +1,29 @@
 import math
 import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+
+
+def plot_converge(clusteres, means):
+    plt.cla()  # 清除原有图像
+
+    plt.title("k-meas converge process")
+    plt.xlabel("density")
+    plt.ylabel("sugar content")
+
+    for cluster in clusteres:
+        cluster = np.array(cluster)
+        # 画出每个簇的点
+        plt.scatter(cluster[:, 0], cluster[:, 1], c="lightcoral")
+        # 画出每个簇的凸包
+        hull = ConvexHull(cluster).vertices.tolist()
+        hull.append(hull[0])
+        plt.plot(cluster[hull, 0], cluster[hull, 1], "c--")
+    # 标记中心点
+    # plt.scatter(means[:, 0], means[:, 1], label="initial center", c="k")
+    # plt.legend()
+    plt.pause(0.5)
 
 
 def euclidean_distance(data1, data2):
@@ -9,28 +33,35 @@ def euclidean_distance(data1, data2):
 
 
 def kmeans(file_path, k, initial_points):
-    # 保存每簇信息
-    clusteres = [[] for _ in range(k)]
     # 读取要聚类的数据
     dataset = np.loadtxt(file_path, delimiter=",")
     # 均值向量
-    means = [dataset[initial_point - 1] for initial_point in initial_points]
+    means = [list(dataset[initial_point - 1]) for initial_point in initial_points]
     means_pre = []
+    # 打开交互模式
+    plt.ion()
     # 聚类
     while means != means_pre:
-        means_pre = means[:]
+        means_pre = copy.deepcopy(means)
+        # 保存每簇信息
+        clusteres = [[] for _ in range(k)]
         for data in dataset:
             # 与每个均值向量计算距离
             euclidean_distances = [euclidean_distance(data, mean) for mean in means]
             # 归到距离最小的簇
             min_index = euclidean_distances.index(min(euclidean_distances))
             clusteres[min_index].append(data)
+        plot_converge(clusteres, means)
         # 更新均值向量
-        means = [np.mean(cluster, axis=0) for cluster in clusteres]
+        means = [list(np.mean(cluster, axis=0)) for cluster in clusteres]
+        print("means:{}".format(means))
+    # 关闭交互模式
+    plt.ioff()
+    plt.show()
 
 
 if __name__ == "__main__":
     file_path = "watermelon4_0_Ch.txt"  # 数据路径
     k = 3  # 聚类簇数
-    initial_points = [6, 12, 27]  # 初始点
+    initial_points = [1, 10, 29]  # 初始点
     kmeans(file_path, k, initial_points)
